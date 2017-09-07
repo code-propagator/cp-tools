37aa0330215ddc2a2eae8a0c6a7d3be547ab50ff
code-propagator <code.propagator@gmail.com>
2017-09-08 08:23:42 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 'use strict'
 
-module.export.modules = {
-  autoUpdater: require('./libcptools/electron/autoUpdater'),
-  createWindow: require('./libcptools/electron/createWindow'),
-  binaryUtil: require('./libcptools/node/binaryUtil'),
-  emitter: require('./libcptools/node/emitter'),
-  hashHist: require('./libcptools/node/hashHist'),
-  timeUtil: require('./libcptools/node/timeUtil'),
-  ClickCount: require('./libcptools/react/ClickCount'),
-  realmUtil: require('./libcptools/realm/realmUtil'),
-  schema: require('./libcptools/realm/schema')
+let binaryUtil = require('./libcptools/node/binaryUtil')
+let emitter = require('./libcptools/node/emitter')
+let hashHist = require('./libcptools/node/hashHist')
+let timeUtil = require('./libcptools/node/timeUtil')
+let ClickCount = require('./libcptools/react/ClickCount')
+
+/*
+### loading electron fails inside node_module
+###
+autoUpdater: require('./libcptools/electron/autoUpdater'),
+createWindow: require('./libcptools/electron/createWindow'),
+*/
+/*
+### Because loading realm inside node_module fails,
+### tools for realm is not installed here.
+realmUtil: require('./libcptools/realm/realmUtil'),
+schema: require('./libcptools/realm/schema'),
+*/
+
+module.exports = {
+  binaryUtil: binaryUtil,
+  emitter: emitter,
+  hashHist: hashHist,
+  timeUtil: timeUtil,
+  ClickCount: ClickCount
 }
